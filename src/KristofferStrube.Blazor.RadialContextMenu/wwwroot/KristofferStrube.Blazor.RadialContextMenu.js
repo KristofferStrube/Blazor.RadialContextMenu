657647cf79fa33489e527654f06e9d07c197c200
Kristoffer Strube <kristoffer.strube@gmail.com>
2021-06-06 17:52:16 +0200

    removed touch press for now

--- src/KristofferStrube.Blazor.RadialContextMenu/wwwroot/KristofferStrube.Blazor.RadialContextMenu.js
+++ src/KristofferStrube.Blazor.RadialContextMenu/wwwroot/KristofferStrube.Blazor.RadialContextMenu.js
@@ -9,12 +9,6 @@ export function Init(ObjRef) {
             currentObjRef.invokeMethodAsync("Close");
         }
     });
-    document.addEventListener("touchend", function (e) {
-        const menuItemsIntersection = e.path.filter(value => currentContextMenuElements.includes(value));
-        if (menuItemsIntersection.length == 0) {
-            currentObjRef.invokeMethodAsync("Close");
-        }
-    })
 }
 
 export function AddMenuElements(ContextMenuElements) {
